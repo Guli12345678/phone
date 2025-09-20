ca103b43d96474ce271e1bc3587db8e25e186447
Guli12345678 <gulsanammamadieva083@gmail.com>
2025-09-20 22:44:41 +0500

    first commit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
 import { memo } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container flex justify-center mx-auto mt-10 font-semibold font-sans text-3xl">
+      <h2>Go to Phone page ➡️</h2>
+    </div>
+  );
 };
 
 export default memo(Home);
